84fb5182bf4775320e8e6cbc855cc5e9030c9736
Celio Silva <celio@delogic.com.br>
2014-09-12 16:09:51 -0300

    Included logging

--- src/main/java/com/bazaarvoice/maven/plugins/s3/upload/Permission.java
+++ src/main/java/com/bazaarvoice/maven/plugins/s3/upload/Permission.java
@@ -11,84 +11,84 @@ import com.amazonaws.services.s3.model.GroupGrantee;
 
 public class Permission implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	/**
-	 * AWS Grantee.
-	 */
-	@Parameter
-	private String grantee;
+    private static final long serialVersionUID = 1L;
 
-	@Parameter(defaultValue = "true")
-	private Boolean download;
+    /**
+     * AWS Grantee.
+     */
+    @Parameter
+    private String            grantee;
 
-	@Parameter(defaultValue = "false")
-	private Boolean viewPermission = Boolean.FALSE;
+    @Parameter(defaultValue = "true")
+    private Boolean           download;
 
-	@Parameter(defaultValue = "false")
-	private Boolean editPermission = Boolean.FALSE;
-	
-	public String getGrantee() {
-		return grantee;
-	}
-	
-	public void setGrantee(String grantee) {
-		this.grantee = grantee;
-	}
-	
-	public Boolean getDownload() {
-		return download;
-	}
-	
-	public void setDownload(Boolean download) {
-		this.download = download;
-	}
-	
-	public Boolean getViewPermission() {
-		return viewPermission;
-	}
-	
-	public void setViewPermission(Boolean viewPermission) {
-		this.viewPermission = viewPermission;
-	}
-	
-	public Boolean getEditPermission() {
-		return editPermission;
-	}
-	
-	public void setEditPermission(Boolean editPermission) {
-		this.editPermission = editPermission;
-	}
-	
-	public Grantee getAsGrantee() {
-		if(grantee.equalsIgnoreCase("Everyone") || grantee.equalsIgnoreCase("allusers")){
-			return GroupGrantee.AllUsers;
-		}
-		if(grantee.equalsIgnoreCase("AuthenticatedUsers")){
-			return GroupGrantee.AuthenticatedUsers;
-		}
-		if(grantee.equalsIgnoreCase("LogDelivery")){
-			return GroupGrantee.LogDelivery;
-		}
-		if(grantee.matches("@[^.]+\\.[^.]+")){
-			return new EmailAddressGrantee(grantee);
-		}
-		return new CanonicalGrantee(grantee);
-	}
-	
-	public com.amazonaws.services.s3.model.Permission getPermission() {
-		if(download && editPermission && viewPermission){
-			return com.amazonaws.services.s3.model.Permission.FullControl;
-		}else{
-			if(editPermission){
-				return com.amazonaws.services.s3.model.Permission.WriteAcp;
-			} else {
-				if(viewPermission){
-					return com.amazonaws.services.s3.model.Permission.ReadAcp;
-				} else {
-					return com.amazonaws.services.s3.model.Permission.Read;
-				}
-			}
-		}
-	}
+    @Parameter(defaultValue = "false")
+    private Boolean           viewPermission   = Boolean.FALSE;
+
+    @Parameter(defaultValue = "false")
+    private Boolean           editPermission   = Boolean.FALSE;
+
+    public String getGrantee() {
+        return grantee;
+    }
+
+    public void setGrantee(String grantee) {
+        this.grantee = grantee;
+    }
+
+    public Boolean getDownload() {
+        return download;
+    }
+
+    public void setDownload(Boolean download) {
+        this.download = download;
+    }
+
+    public Boolean getViewPermission() {
+        return viewPermission;
+    }
+
+    public void setViewPermission(Boolean viewPermission) {
+        this.viewPermission = viewPermission;
+    }
+
+    public Boolean getEditPermission() {
+        return editPermission;
+    }
+
+    public void setEditPermission(Boolean editPermission) {
+        this.editPermission = editPermission;
+    }
+
+    public Grantee getAsGrantee() {
+        if (grantee.equalsIgnoreCase("Everyone") || grantee.equalsIgnoreCase("allusers")) {
+            return GroupGrantee.AllUsers;
+        }
+        if (grantee.equalsIgnoreCase("AuthenticatedUsers")) {
+            return GroupGrantee.AuthenticatedUsers;
+        }
+        if (grantee.equalsIgnoreCase("LogDelivery")) {
+            return GroupGrantee.LogDelivery;
+        }
+        if (grantee.matches("@[^.]+\\.[^.]+")) {
+            return new EmailAddressGrantee(grantee);
+        }
+        return new CanonicalGrantee(grantee);
+    }
+
+    public com.amazonaws.services.s3.model.Permission getPermission() {
+        if (download && editPermission && viewPermission) {
+            return com.amazonaws.services.s3.model.Permission.FullControl;
+        } else {
+            if (editPermission) {
+                return com.amazonaws.services.s3.model.Permission.WriteAcp;
+            } else {
+                if (viewPermission) {
+                    return com.amazonaws.services.s3.model.Permission.ReadAcp;
+                } else {
+                    return com.amazonaws.services.s3.model.Permission.Read;
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/bazaarvoice/maven/plugins/s3/upload/S3UploadMojo.java
+++ src/main/java/com/bazaarvoice/maven/plugins/s3/upload/S3UploadMojo.java
@@ -2,9 +2,6 @@ package com.bazaarvoice.maven.plugins.s3.upload;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.LinkedList;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -26,175 +23,180 @@ import com.amazonaws.services.s3.transfer.TransferManager;
 
 @Mojo(name = "s3-upload")
 public class S3UploadMojo extends AbstractMojo {
-	/** Access key for S3. */
-	@Parameter(property = "s3-upload.accessKey")
-	private String accessKey;
+    /** Access key for S3. */
+    @Parameter(property = "s3-upload.accessKey")
+    private String                 accessKey;
 
-	/** Secret key for S3. */
-	@Parameter(property = "s3-upload.secretKey")
-	private String secretKey;
+    /** Secret key for S3. */
+    @Parameter(property = "s3-upload.secretKey")
+    private String                 secretKey;
 
-	/**
-	 * Execute all steps up except the upload to the S3. This can be set to true
-	 * to perform a "dryRun" execution.
-	 */
-	@Parameter(property = "s3-upload.doNotUpload", defaultValue = "false")
-	private boolean doNotUpload;
+    /**
+     * Execute all steps up except the upload to the S3. This can be set to true
+     * to perform a "dryRun" execution.
+     */
+    @Parameter(property = "s3-upload.doNotUpload", defaultValue = "false")
+    private boolean                doNotUpload;
 
-	/**
-	 * Skips execution
-	 */
-	@Parameter(property = "s3-upload.skip", defaultValue = "false")
-	private boolean skip;
+    /**
+     * Skips execution
+     */
+    @Parameter(property = "s3-upload.skip", defaultValue = "false")
+    private boolean                skip;
 
-	/** The file/folder to upload. */
-	@Parameter(property = "s3-upload.source", required = true)
-	private String source;
+    /** The file/folder to upload. */
+    @Parameter(property = "s3-upload.source", required = true)
+    private String                 source;
 
-	/** The bucket to upload into. */
-	@Parameter(property = "s3-upload.bucketName", required = true)
-	private String bucketName;
+    /** The bucket to upload into. */
+    @Parameter(property = "s3-upload.bucketName", required = true)
+    private String                 bucketName;
 
-	/** The file/folder (in the bucket) to create. */
-	@Parameter(property = "s3-upload.destination", required = true)
-	private String destination;
+    /** The file/folder (in the bucket) to create. */
+    @Parameter(property = "s3-upload.destination", required = true)
+    private String                 destination;
 
-	/** Force override of endpoint for S3 regions such as EU. */
-	@Parameter(property = "s3-upload.endpoint")
-	private String endpoint;
+    /** Force override of endpoint for S3 regions such as EU. */
+    @Parameter(property = "s3-upload.endpoint")
+    private String                 endpoint;
 
-	/** In the case of a directory upload, recursively upload the contents. */
-	@Parameter(property = "s3-upload.recursive", defaultValue = "false")
-	private boolean recursive;
+    /** In the case of a directory upload, recursively upload the contents. */
+    @Parameter(property = "s3-upload.recursive", defaultValue = "false")
+    private boolean                recursive;
 
-	@Parameter(property = "s3-upload.permissions")
-	private LinkedList<Permission> permissions;
+    @Parameter(property = "s3-upload.permissions")
+    private LinkedList<Permission> permissions;
 
-	@Parameter(property = "s3-upload.metadatas")
-	private LinkedList<Metadata> metadatas;
+    @Parameter(property = "s3-upload.metadatas")
+    private LinkedList<Metadata>   metadatas;
 
-	@Override
-	public void execute() throws MojoExecutionException {
-		if (skip) {
-			getLog().info("Skipping S3UPload");
-			return;
-		}
-		File sourceFile = new File(source);
-		if (!sourceFile.exists()) {
-			throw new MojoExecutionException("File/folder doesn't exist: " + source);
-		}
+    @Override
+    public void execute() throws MojoExecutionException {
+        if (skip) {
+            getLog().info("Skipping S3UPload");
+            return;
+        }
+        File sourceFile = new File(source);
+        if (!sourceFile.exists()) {
+            throw new MojoExecutionException("File/folder doesn't exist: " + source);
+        }
 
-		AmazonS3 s3 = getS3Client(accessKey, secretKey);
-		if (endpoint != null) {
-			s3.setEndpoint(endpoint);
-		}
+        AmazonS3 s3 = getS3Client(accessKey, secretKey);
+        if (endpoint != null) {
+            s3.setEndpoint(endpoint);
+        }
 
-		if (!s3.doesBucketExist(bucketName)) {
-			throw new MojoExecutionException("Bucket doesn't exist: " + bucketName);
-		}
+        if (!s3.doesBucketExist(bucketName)) {
+            throw new MojoExecutionException("Bucket doesn't exist: " + bucketName);
+        }
 
-		if (doNotUpload) {
-			getLog().info(String.format("File %s would have be uploaded to s3://%s/%s (dry run)", sourceFile, bucketName, destination));
+        if (doNotUpload) {
+            getLog().info(String.format("File %s would have be uploaded to s3://%s/%s (dry run)", sourceFile, bucketName, destination));
 
-			return;
-		}
-		
-		boolean success = upload(s3, sourceFile);
-		if (!success) {
-			throw new MojoExecutionException("Unable to upload file to S3.");
-		}
+            return;
+        }
 
-		getLog().info(String.format("File %s uploaded to s3://%s/%s", sourceFile, bucketName, destination));
-	}
+        boolean success = upload(s3, sourceFile);
+        if (!success) {
+            throw new MojoExecutionException("Unable to upload file to S3.");
+        }
 
-	private static AmazonS3 getS3Client(String accessKey, String secretKey) {
-		AWSCredentialsProvider provider;
-		if (accessKey != null && secretKey != null) {
-			AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-			provider = new StaticCredentialsProvider(credentials);
-		} else {
-			provider = new DefaultAWSCredentialsProviderChain();
-		}
+        getLog().info(String.format("File %s uploaded to s3://%s/%s", sourceFile, bucketName, destination));
+    }
 
-		return new AmazonS3Client(provider);
-	}
+    private static AmazonS3 getS3Client(String accessKey, String secretKey) {
+        AWSCredentialsProvider provider;
+        if (accessKey != null && secretKey != null) {
+            AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+            provider = new StaticCredentialsProvider(credentials);
+        } else {
+            provider = new DefaultAWSCredentialsProviderChain();
+        }
 
-	private boolean upload(AmazonS3 s3, File sourceFile) throws MojoExecutionException {
-		TransferManager mgr = new TransferManager(s3);
+        return new AmazonS3Client(provider);
+    }
 
-		Transfer transfer;
-		if (sourceFile.isFile()) {
-			transfer = mgr.upload(bucketName, destination, sourceFile);
-		} else if (sourceFile.isDirectory()) {
-			transfer = mgr.uploadDirectory(bucketName, destination, sourceFile, recursive);
-		} else {
-			throw new MojoExecutionException("File is neither a regular file nor a directory " + sourceFile);
-		}
-		try {
-			getLog().debug("Transferring " + transfer.getProgress().getTotalBytesToTransfer() + " bytes...");
-			transfer.waitForCompletion();
-			getLog().info("Transferred " + transfer.getProgress().getBytesTransfered() + " bytes.");
+    private boolean upload(AmazonS3 s3, File sourceFile) throws MojoExecutionException {
+        TransferManager mgr = new TransferManager(s3);
 
-			try {
-				if(metadatas != null && metadatas.size() > 0){
-					updateMetadatas(s3, sourceFile,sourceFile.getCanonicalPath(),destination);
-				}
-				if(permissions != null && permissions.size() > 0){
-					updatePermissions(s3,sourceFile,sourceFile.getCanonicalPath(),destination);
-				}
-			} catch (IOException e) {
-				throw new MojoExecutionException("Error getting file canonicalPath when updating permissions/metadatas",e);
-			}
-		} catch (InterruptedException e) {
-			return false;
-		}
+        Transfer transfer;
+        if (sourceFile.isFile()) {
+            transfer = mgr.upload(bucketName, destination, sourceFile);
+        } else if (sourceFile.isDirectory()) {
+            transfer = mgr.uploadDirectory(bucketName, destination, sourceFile, recursive);
+        } else {
+            throw new MojoExecutionException("File is neither a regular file nor a directory " + sourceFile);
+        }
+        try {
+            getLog().debug("Transferring " + transfer.getProgress().getTotalBytesToTransfer() + " bytes...");
+            transfer.waitForCompletion();
+            getLog().info("Transferred " + transfer.getProgress().getBytesTransfered() + " bytes.");
+            try {
+                if (metadatas != null && metadatas.size() > 0) {
+                    updateMetadatas(s3, sourceFile, sourceFile.getCanonicalPath(), destination);
+                }
+                if (permissions != null && permissions.size() > 0) {
+                    updatePermissions(s3, sourceFile, sourceFile.getCanonicalPath(), destination);
+                }
+            } catch (IOException e) {
+                throw new MojoExecutionException("Error getting file canonicalPath when updating permissions/metadatas", e);
+            }
+        } catch (InterruptedException e) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	private void updatePermissions(AmazonS3 s3, File sourceFile, String localPrefix, String keyPrefix) throws MojoExecutionException {
-		try{
-			if (sourceFile.isFile()) {
-				updatePermissions(s3, sourceFile.getCanonicalPath().replace(localPrefix, keyPrefix));
-			} else {
-				for(File f:sourceFile.listFiles()){
-					updatePermissions(s3, f, f.getCanonicalPath(), keyPrefix.replaceAll("([^/])/*$","$1/")+f.getName());
-				}
-			}
-		}catch(IOException ioe){
-			throw new MojoExecutionException("Error getting file canonicalPath when updating permissions",ioe);
-		}
-	}
-	private void updatePermissions(AmazonS3 s3, String key) {
-		AccessControlList acl = s3.getObjectAcl(bucketName, key);
-		for(Permission p :permissions){
-			acl.grantPermission(p.getAsGrantee(), p.getPermission());
-		}
-		s3.setObjectAcl(bucketName, key, acl);
-//		getLog().info("Updating permissions for '"+key+"' in bucket '"+bucketName);
-	}
+    private void updatePermissions(AmazonS3 s3, File sourceFile, String localPrefix, String keyPrefix) throws MojoExecutionException {
+        try {
+            if (sourceFile.isFile()) {
+                updatePermissions(s3, sourceFile.getCanonicalPath().replace(localPrefix, keyPrefix));
+            } else {
+                for (File f : sourceFile.listFiles()) {
+                    updatePermissions(s3, f, f.getCanonicalPath(), keyPrefix.replaceAll("([^/])/*$", "$1/") + f.getName());
+                }
+            }
+        } catch (IOException ioe) {
+            throw new MojoExecutionException("Error getting file canonicalPath when updating permissions", ioe);
+        }
+    }
 
-	private void updateMetadatas(AmazonS3 s3, File sourceFile, String localPrefix, String keyPrefix) throws MojoExecutionException {
-		try{
-			if (sourceFile.isFile()) {
-				updateMetadatas(s3, sourceFile.getCanonicalPath().replace(localPrefix, keyPrefix));
-			} else {
-				for(File f:sourceFile.listFiles()){
-					updateMetadatas(s3, f, f.getCanonicalPath(), keyPrefix.replaceAll("([^/])/*$","$1/")+f.getName());
-				}
-			}
-		}catch(IOException ioe){
-			throw new MojoExecutionException("Error getting file canonicalPath when updating metadatas",ioe);
-		}
-	}
+    private void updatePermissions(AmazonS3 s3, String key) {
+        AccessControlList acl = s3.getObjectAcl(bucketName, key);
+        for (Permission p : permissions) {
+            acl.grantPermission(p.getAsGrantee(), p.getPermission());
+        }
+        s3.setObjectAcl(bucketName, key, acl);
+        if (getLog().isInfoEnabled()) {
+            getLog().info("Updating permissions for '" + key + "' in bucket '" + bucketName);
+        }
+    }
 
-	private void updateMetadatas(AmazonS3 s3, String key) throws MojoExecutionException {
-		S3Object s3o = s3.getObject(bucketName, key);
-		for (Metadata m: metadatas) {
-			if(m.shouldSetMetadata(key)){
-				s3o.getObjectMetadata().setHeader(m.getKey(), m.getValue());
-			}
-		}
-		s3.putObject(bucketName, key, s3o.getObjectContent(), s3o.getObjectMetadata());
-	}
+    private void updateMetadatas(AmazonS3 s3, File sourceFile, String localPrefix, String keyPrefix) throws MojoExecutionException {
+        try {
+            if (sourceFile.isFile()) {
+                updateMetadatas(s3, sourceFile.getCanonicalPath().replace(localPrefix, keyPrefix));
+            } else {
+                for (File f : sourceFile.listFiles()) {
+                    updateMetadatas(s3, f, f.getCanonicalPath(), keyPrefix.replaceAll("([^/])/*$", "$1/") + f.getName());
+                }
+            }
+        } catch (IOException ioe) {
+            throw new MojoExecutionException("Error getting file canonicalPath when updating metadatas", ioe);
+        }
+    }
+
+    private void updateMetadatas(AmazonS3 s3, String key) throws MojoExecutionException {
+        S3Object s3o = s3.getObject(bucketName, key);
+        for (Metadata m : metadatas) {
+            if (m.shouldSetMetadata(key)) {
+                s3o.getObjectMetadata().setHeader(m.getKey(), m.getValue());
+            }
+        }
+        if (getLog().isDebugEnabled()) {
+            getLog().debug("setting metadata:" + s3o.getObjectMetadata().getRawMetadata() + " for object " + bucketName + ":" + key);
+        }
+        s3.putObject(bucketName, key, s3o.getObjectContent(), s3o.getObjectMetadata());
+    }
 }
